01b86a8a60536d189c271096fa6910ab1e232b5e
Peter Dalbhanjan <dalbhanj@amazon.com>
2022-10-07 14:21:20 -0400

    adding customPreCompare to resolve comaprison errors with ConfigurationOverrides

--- pkg/resource/job_run/hooks.go
+++ pkg/resource/job_run/hooks.go
@@ -29,26 +29,47 @@ func stringToConfigurationOverrides(cfg *string) (*svcsdk.ConfigurationOverrides
 
 func customPreCompare(
 	delta *ackcompare.Delta,
-	// cfg *svcsdk.ConfigurationOverrides,
 	a *resource,
 	b *resource,
 ) {
+	aConfig, err := stringToConfigurationOverrides(a.ko.Spec.ConfigurationOverrides)
+	if err != nil {
+		panic(err)
+	}
+	bConfig, err := stringToConfigurationOverrides(b.ko.Spec.ConfigurationOverrides)
+	if err != nil {
+		panic(err)
+	}
+
 	if ackcompare.HasNilDifference(a.ko.Spec.ConfigurationOverrides, b.ko.Spec.ConfigurationOverrides) {
 		delta.Add("Spec.ConfigurationOverrides", a.ko.Spec.ConfigurationOverrides, b.ko.Spec.ConfigurationOverrides)
 	} else if a.ko.Spec.ConfigurationOverrides != nil && b.ko.Spec.ConfigurationOverrides != nil {
-		if ackcompare.HasNilDifference(a.ko.Spec.ConfigurationOverrides.ApplicationConfiguration, b.ko.Spec.ConfigurationOverrides.ApplicationConfiguration) {
-			delta.Add("Spec.ConfigurationOverrides.ApplicationConfiguration", a.ko.Spec.ConfigurationOverrides.ApplicationConfiguration, b.ko.Spec.ConfigurationOverrides.ApplicationConfiguration)
-		} else if a.ko.Spec.ConfigurationOverrides.ApplicationConfiguration != nil && b.ko.Spec.ConfigurationOverrides.ApplicationConfiguration != nil {
-			if *a.ko.Spec.ConfigurationOverrides.ApplicationConfiguration != *b.ko.Spec.ConfigurationOverrides.ApplicationConfiguration {
-				delta.Add("Spec.ConfigurationOverrides.ApplicationConfiguration", a.ko.Spec.ConfigurationOverrides.ApplicationConfiguration, b.ko.Spec.ConfigurationOverrides.ApplicationConfiguration)
-			}
-		}
-		if ackcompare.HasNilDifference(a.ko.Spec.ConfigurationOverrides.MonitoringConfiguration, b.ko.Spec.ConfigurationOverrides.MonitoringConfiguration) {
-			delta.Add("Spec.ConfigurationOverrides.MonitoringConfiguration", a.ko.Spec.ConfigurationOverrides.MonitoringConfiguration, b.ko.Spec.ConfigurationOverrides.MonitoringConfiguration)
-		} else if a.ko.Spec.ConfigurationOverrides.MonitoringConfiguration != nil && b.ko.Spec.ConfigurationOverrides.MonitoringConfiguration != nil {
-			if *a.ko.Spec.ConfigurationOverrides.MonitoringConfiguration != *b.ko.Spec.ConfigurationOverrides.MonitoringConfiguration {
-				delta.Add("Spec.ConfigurationOverrides.MonitoringConfiguration", a.ko.Spec.ConfigurationOverrides.MonitoringConfiguration, b.ko.Spec.ConfigurationOverrides.MonitoringConfiguration)
-			}
-		}
+		delta.Add("Spec.ConfigurationOverrides", a.ko.Spec.ConfigurationOverrides, b.ko.Spec.ConfigurationOverrides)
 	}
-}
+
+	if ackcompare.HasNilDifference(aConfig.ApplicationConfiguration, bConfig.ApplicationConfiguration) {
+		delta.Add("Spec.ConfigurationOverrides", aConfig.ApplicationConfiguration, bConfig.ApplicationConfiguration)
+	} else if aConfig.ApplicationConfiguration != nil && bConfig.ApplicationConfiguration != nil {
+		delta.Add("Spec.ConfigurationOverrides", aConfig.ApplicationConfiguration, bConfig.ApplicationConfiguration)
+	} else if aConfig.ApplicationConfiguration != nil && *bConfig.ApplicationConfiguration == "null" {
+		delta.Add("Spec.ConfigurationOverrides", aConfig.ApplicationConfiguration, bConfig.ApplicationConfiguration)
+	}
+
+	if ackcompare.HasNilDifference(aConfig.MonitoringConfiguration, bConfig.MonitoringConfiguration) {
+		delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration, bConfig.MonitoringConfiguration)
+	} else if aConfig.MonitoringConfiguration != nil && bConfig.MonitoringConfiguration !=nil {
+		if ackcompare.HasNilDifference(aConfig.MonitoringConfiguration.PersistentAppUI, bConfig.MonitoringConfiguration.PersistentAppUI) {
+			delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.PersistentAppUI, bConfig.MonitoringConfiguration.PersistentAppUI)
+		} else if aConfig.MonitoringConfiguration.PersistentAppUI != nil && bConfig.MonitoringConfiguration.PersistentAppUI != nil {
+			delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.PersistentAppUI, bConfig.MonitoringConfiguration.PersistentAppUI)
+		} else if aConfig.MonitoringConfiguration.PersistentAppUI != nil && *bConfig.MonitoringConfiguration.PersistentAppUI == "ENABLED" {
+			delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.PersistentAppUI, bConfig.MonitoringConfiguration.PersistentAppUI)
+		}
+		} else if ackcompare.HasNilDifference(aConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration, bConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration) {
+			delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration, bConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration)
+		} else if aConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration != nil && bConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration != nil { delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration, bConfig.MonitoringConfiguration.CloudWatchMonitoringConfiguration)
+		} else if ackcompare.HasNilDifference(aConfig.MonitoringConfiguration.S3MonitoringConfiguration, bConfig.MonitoringConfiguration.S3MonitoringConfiguration) {
+				delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.S3MonitoringConfiguration, bConfig.MonitoringConfiguration.S3MonitoringConfiguration)
+				} else if aConfig.MonitoringConfiguration.S3MonitoringConfiguration != nil && bConfig.MonitoringConfiguration.S3MonitoringConfiguration != nil { delta.Add("Spec.ConfigurationOverrides", aConfig.MonitoringConfiguration.S3MonitoringConfiguration, bConfig.MonitoringConfiguration.S3MonitoringConfiguration)
+			}
+  }
